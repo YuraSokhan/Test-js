367fb085ca048af1419465a1bb809275281f0353
Yura <sokhanyuriy@gmail.com>
2026-09-19 16:28:03 +0300

    objects have been learned

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-console.log('hello, niggers');
+console.log('hello, world');
 const girl = (`
 ___███████▀◢▆▅▃ 　　　   　　 　　　 ▀▀████
 ___██████▌◢▀█▓▓█◣   　　　　　　▂▃▃　 ████
@@ -16,12 +16,12 @@ console.log(girl);
 
 let user12;
 let userName;
-userName = "Nigga";
+userName = "Hello";
 user12 = userName;
 console.log(user12, userName);
 
 let name1  = {
-    name: "453DFG" ,
+    name: "453DFG",
     age: 35
 }
 console.log(name1);
@@ -30,7 +30,7 @@ user2 = Number(user2);
 console.log(user2);
 
 let a;
-a = null || 0 || undefined || (true * 5) || "nigga" || 3 ;   // =5
+a = null || 0 || undefined || (true * 5) || "hello" || 3 ;   // =5
 console.log(a);
 
 let b = "null" && 1 && undefined && 2;  // undefined
@@ -45,7 +45,7 @@ console.log(10 > 5 && 10 === 1);
 
 let i = -10;
 while (2 > i) {
- console.log("nigga 12 times");
+ console.log("hello 12 times");
  i++;
 }
 
@@ -71,7 +71,7 @@ FirstFor: for(let num = 0; num < 4; num++){           //мітка для break 
 
 let message;
 function showMessage() {                             
-    message = 14;                                   //про особливості глобаних, локальних змінних, можна побачити з виводом в консоль
+    message = 14;                                   //про особливості глобальних, локальних змінних, можна побачити з виводом в консоль
 }
 console.log(message);
 showMessage();
@@ -87,7 +87,7 @@ console.log(message1);
 
 function calcSum(numOne = 0, numTwo = 0){           // можна присвоїти значення для параметра
     let numSum = numOne + numTwo;
-    console.log('Сума: ' + ((numSum == true) ? numSum : "Enter something nigga"))
+    console.log('Сума: ' + ((numSum == true) ? numSum : "Enter something"))
 }
 calcSum()                    // а ще можна не задати параметри, навіть якщо треба, але буде тоді значення undefined змінної
 
@@ -99,10 +99,10 @@ function calcSumMod(numOne, numTwo, less, more){  // отак тоже можн�
     }    
 }
 function showLessMassage(){
-    console.log("short number nigga");
+    console.log("short number ");
 }
 function showMoreMessage(){
-    console.log("long number nigga");
+    console.log("long number ");
 }
 calcSumMod(1, 2, showLessMassage, showMoreMessage);  // showMoreMessage, а не showMoreMessage(), бо викличеш функцію зразу
 
@@ -123,9 +123,104 @@ fun();
 function showMessage3 (text, count = 0){
     console.log(text);
     if (count < 5) {
-        setTimeout(showMessage3, 500, text+" nigga", count + 1);       // повторює виклик функції, але є обмеження через умову
+        setTimeout(showMessage3, 500, text+" hello", count + 1);       // повторює виклик функції, але є обмеження через умову
     }    
 }
-let timeId = setTimeout(showMessage3, 1000, "nigga");                 // викликає функцію з інтервалом часу 1000мл секунд
+let timeId = setTimeout(showMessage3, 1000, "hello");                 // викликає функцію з інтервалом часу 1000мл секунд
 clearTimeout(timeId);  // зупиняє setTimeout
+console.log("---------------------------------OBJECT------------------------------");
 
+// обєкти
+let car = {
+    brand: "BMW", 
+    model: 7,
+    adress: {
+        state: "California",
+        city: "Los Angeles",
+    }
+}
+console.log(car.brand);
+console.log(car.adress);
+console.log(car.adress.state);   // обєкт в обєкті
+console.log("--------------------------------------------------------------------");
+
+car.adress.country = "USA";     // створенння нової властивості обєкта
+console.log(car.adress);
+delete car.adress.country;      // видалення властивості
+console.log(car.adress);
+console.log("--------------------------------------------------------------------");
+
+
+let anotherCar = car;                   // це не створення нового обєкта, а створення "посилання, ярлика" на основний обєкт car
+console.log(anotherCar);
+//anotherCar.brand = "AUDI";             // не дивлячись що ми змінили anotherCar, car так само змінилась
+console.log(car);                      
+console.log("--------------------------------------------------------------------");
+
+let newCar = Object.assign({year: 2021}, car);       // оце вже створення нового обєкта, на основі іншого обєкта
+newCar.brand = "Mersedec";                  //  він вже функціонує, як незалежно інший обєкт
+console.log(car);
+console.log(newCar);
+console.log("--------------------------------------------------------------------");
+
+console.log(car.year);                // якщо я хочу вивести допустим неіснуючу властивість - undefined
+console.log(car?.year?.old)           // якщо є вкладеність, то вибє помилку, тому треба писати ?
+if("year" in car) {
+    console.log(car.year);            // провірка чи є car.year. in корисне у випадку, якщо змінна існує, але має значенння undefined
+}
+console.log("--------------------------------------------------------------------");
+
+for(let key in car) {                  // цикл for in, щоб переглянути всі властивості обєкта
+    console.log(key);
+    console.log(car[key]);
+}
+console.log("--------------------------------------------------------------------");
+
+for(let key in car.adress) {            // цикл для пергляду обєктів в обєкті 
+    console.log(key);
+    console.log(car.adress[key]);
+}
+console.log("--------------------------------------------------------------------");
+
+let car1 = {
+    brand: "AUDI", 
+    model: 7,
+    adress: "Los Angeles",
+
+    showInfo() {                                         // це метод обєкта
+        for(let key in car1) {                                  
+            if (typeof this[key] !== 'function'){       // і тут такий прикол шо for in так само виводить методи, а не тільки властивості
+                console.log(`${key}: ${car1[key]}`);
+            }
+        }
+    }
+}
+car1.showInfo();
+console.log("--------------------------------------------------------------------");
+
+let car2 = {          // це посилання на обєкт, а не сам обєкт, якщо обєкт вже немає змін-посилань на себе, то він самознищиться
+    brand: "Opel",
+    model: 3,
+    adress: "New York City",
+
+    showInfo () {
+        console.log(`brand: ${this.brand},      model: ${this.model},     adress: ${this.adress}`);     
+        // так ось в чому логіка, якщо змінна-посилання на обєкт car2 буде зміненна то запис car2.model очевидно вже буде не дійсне, тому тут доречне this
+        // console.log(`brand: ${car2.brand},      model: ${car2.model},     adress: ${car2.adress}`);
+    }   
+}
+let car3 = car2;
+car2 = null;
+car3.showInfo()
+console.log("--------------------------------------------------------------------");
+
+function UserInfo (name, age = 30) {      // конструктора імя з великої букви
+    this.name = name;                     // this бо потім зміниться імя
+    this.age = age;
+}
+
+console.log(new UserInfo("hell naa", 23));
+console.log(new UserInfo("hello"));
+
+let variable = new UserInfo("helo");
+console.log(variable);
